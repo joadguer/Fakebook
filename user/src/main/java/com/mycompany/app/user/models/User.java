5dbc62944cf595e45685e6afc6169e37eef4c26f
joadguer <josueadrianib@gmail.com>
2025-05-20 19:51:40 -0500

    initial setup

--- user/src/main/java/com/mycompany/app/user/models/User.java
+++ user/src/main/java/com/mycompany/app/user/models/User.java
@@ -3,6 +3,21 @@ package com.mycompany.app.user.models;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+
+@Builder
+@AllArgsConstructor
+@Getter
+@ToString
 @Entity
 @Table(name = "users")
 public class User {
@@ -27,63 +42,6 @@ public class User {
     private boolean activo;
 
     // --- Constructores ---
-    public User() {}
+ 
 
-    public User(String email, String nombre, String contraseña) {
-        this.email = email;
-        this.nombre = nombre;
-        this.contraseña = contraseña;
-        this.fechaRegistro = LocalDateTime.now();
-        this.activo = true;
-    }
-
-    // --- Getters y Setters ---
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-
-    public LocalDateTime getFechaRegistro() {
-        return fechaRegistro;
-    }
-
-    public void setFechaRegistro(LocalDateTime fechaRegistro) {
-        this.fechaRegistro = fechaRegistro;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
 }
