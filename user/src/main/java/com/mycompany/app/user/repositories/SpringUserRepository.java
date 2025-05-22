8368089c125a85f02adbe6ad33bc79451c10ff94
joadguer <josueadrianib@gmail.com>
2025-05-21 19:19:49 -0500

    User Repo

--- user/src/main/java/com/mycompany/app/user/repositories/SpringUserRepository.java
+++ user/src/main/java/com/mycompany/app/user/repositories/SpringUserRepository.java
@@ -1,5 +1,35 @@
 package com.mycompany.app.user.repositories;
 
-public class SpringUserRepository implements UserRepository{
-    
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mycompany.app.user.exceptions.UserAlreadyRegistered;
+import com.mycompany.app.user.exceptions.UserNotFound;
+import com.mycompany.app.user.models.User;
+
+@Repository
+public class SpringUserRepository implements UserRepository {
+
+    @Autowired
+    private InnerSpringUserRepository innerSpringUserRepository;
+
+    @Override
+    public User find(String email) throws UserNotFound {
+        return innerSpringUserRepository.findById(email)
+                .orElseThrow(() -> new UserNotFound("User with email " + email + " not found."));
+    }
+
+    @Override
+    public void save(User user) throws UserAlreadyRegistered{
+        if (!innerSpringUserRepository.findById(user.getEmail()).isEmpty()) {
+            throw new UserAlreadyRegistered("User with email " + user.getEmail() + " is already registered.");
+        }
+        innerSpringUserRepository.save(user);
+    }
+
+}
+interface InnerSpringUserRepository extends JpaRepository<User, String> {
+
 }
--- user/src/main/java/com/mycompany/app/user/repositories/UserRepository.java
+++ user/src/main/java/com/mycompany/app/user/repositories/UserRepository.java
@@ -1,5 +1,10 @@
 package com.mycompany.app.user.repositories;
 
+import com.mycompany.app.user.exceptions.UserAlreadyRegistered;
+import com.mycompany.app.user.exceptions.UserNotFound;
+import com.mycompany.app.user.models.User;
+
 public interface UserRepository {
-    
+    User find(String email) throws UserNotFound;
+    void save(User user) throws UserAlreadyRegistered;
 }
