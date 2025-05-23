a6361a50d522ca60ca4b4a4a3c58c92eb1a43f7f
unknown <erillope@espol.edu.ec>
2025-05-22 20:57:52 -0500

    anotation fix

--- user/src/main/java/com/mycompany/app/user/UserApplication.java
+++ user/src/main/java/com/mycompany/app/user/UserApplication.java
@@ -3,11 +3,9 @@ package com.mycompany.app.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(basePackages = "com.mycompany.app.user")
 public class UserApplication {
 
 	public static void main(String[] args) {
