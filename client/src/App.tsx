4f77d11b034bc0d431b547e41827248e505c716c
unknown <erillope@espol.edu.ec>
2025-05-24 19:18:21 -0500

    init react  project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css'
-import { Formulario3 } from './Formulario3'
-import { useMyForm } from './useMyForm'
 
 function App() {
-  const formController = useMyForm()
   
   return (
-    <Formulario3 {...formController.getProps2()}/>
+    <></>
   )
 }
 
