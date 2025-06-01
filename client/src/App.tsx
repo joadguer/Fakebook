5dbfb567cdcec28ce8dbe774a93150bc10d6b783
unknown <erillope@espol.edu.ec>
2025-05-31 19:27:02 -0500

    router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,14 @@
-import { Email } from '@mui/icons-material'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import { InputText, PasswordInput } from './components/InputText'
-import { useInputText } from './hooks/useInputText'
-import { GenericForm } from './components/GenericForm'
 
 function App() {
-  const inputController = useInputText()
 
   return (
-    <GenericForm onSubmit={() => console.log(inputController.value)}>
-      <InputText label={"Nombre"} focusedLabel='Ingresa tu nombre' icon={<Email sx={{color: '#B0C0C0'}}/>} {...inputController.getProps()}/>
-      <PasswordInput {...inputController.getProps()}/>
-    </GenericForm>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1 />} />
+      </Routes>
+    </Router>
   )
 }
 
